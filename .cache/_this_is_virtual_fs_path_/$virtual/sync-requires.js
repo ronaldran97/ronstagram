8c50f33327d4188ea967fbf39b4c06d43021e8e1
Ronald <ronaldtran97>
2022-09-09 23:15:22 -0700

    maybe this will fix it, did yarn install

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/ronaldtran/ronstagram/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/ronaldtran/ronstagram/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/ronaldtran/ronstagram/src/pages/index.js")),
   "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/ronaldtran/ronstagram/src/pages/using-typescript.tsx")),
